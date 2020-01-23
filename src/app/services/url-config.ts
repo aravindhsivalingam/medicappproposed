050cb28bd0095bd47e45b37ad3948aecd60ad220
AravindhSiva <adamaravindh@gmail.com>
2020-01-23 15:42:34 +0530

    Updated URL

--- src/app/services/url-config.ts
+++ src/app/services/url-config.ts
@@ -5,12 +5,12 @@ export class UrlService {
 
     constructor() { }
 
-    // urlList: any = {
-    //     baseUrl: 'https://node-image-converter.herokuapp.com',
-    // };
     urlList: any = {
-        baseUrl: 'http://localhost:5678',
+        baseUrl: 'https://node-image-converter.herokuapp.com',
     };
+    // urlList: any = {
+    //     baseUrl: 'http://localhost:5678',
+    // };
 
     getBaseUrl() {
         return this.urlList.baseUrl;
